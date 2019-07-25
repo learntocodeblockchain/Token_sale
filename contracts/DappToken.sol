4c2e0d5c0f67ec2b86adcab922b87da5412f6148
manoj sigamani <mjsg3103@hotmail.com>
2019-07-25 13:14:07 +0300

    4.delegate Transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -45,17 +45,17 @@ contract DappToken {
         return true;
     }
 
-    // function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
-    //     require(_value <= balanceOf[_from]);
-    //     require(_value <= allowance[_from][msg.sender]);
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
 
-    //     balanceOf[_from] -= _value;
-    //     balanceOf[_to] += _value;
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
 
-    //     allowance[_from][msg.sender] -= _value;
+        allowance[_from][msg.sender] -= _value;
 
-    //     emit Transfer(_from, _to, _value);
+        emit Transfer(_from, _to, _value);
 
-    //     return true;
-    // }
+        return true;
+    }
 }
