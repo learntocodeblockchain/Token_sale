2d7576b116b1ef043851eab1c64d772a37db9b6d
manoj sigamani <mjsg3103@hotmail.com>
2019-07-25 13:10:43 +0300

    3.Approve Tokens

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -37,13 +37,13 @@ contract DappToken {
         return true;
     }
 
-    // function approve(address _spender, uint256 _value) public returns (bool success) {
-    //     allowance[msg.sender][_spender] = _value;
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        allowance[msg.sender][_spender] = _value;
 
-    //     emit Approval(msg.sender, _spender, _value);
+        emit Approval(msg.sender, _spender, _value);
 
-    //     return true;
-    // }
+        return true;
+    }
 
     // function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
     //     require(_value <= balanceOf[_from]);
